ed1b693782492a2ed4f6c0ff8a9d6674daf3a8fe
KRISHNENDU BIR <23EE01025@iitbbs.ac.in>
2026-03-05 04:16:38 +0530

    change

--- function_by_js/app.js
+++ function_by_js/app.js
@@ -1,27 +1,4 @@
 
-//   // for navbar section
-//   const offcanvasElement = document.getElementById('offcanvasNavbar');
-//   const offcanvas = bootstrap.Offcanvas.getOrCreateInstance(offcanvasElement);
-
-//   document.querySelectorAll('#offcanvasNavbar .nav-link:not(.dropdown-toggle)')
-//     .forEach(link => {
-//       link.addEventListener('click', () => {
-//         offcanvas.hide();   // ✅ Correct way to close
-//       });
-//     });
-// document.querySelectorAll('.offcanvas .nav-link').forEach(link => {
-//   link.addEventListener('click', function () {
-
-//     const offcanvasElement = document.querySelector('.offcanvas.show');
-    
-//     if (offcanvasElement) {
-//       const offcanvas = bootstrap.Offcanvas.getInstance(offcanvasElement);
-//       offcanvas.hide();
-//     }
-
-//   });
-// });
-
 // for navbar section
 const offcanvasElement = document.getElementById('offcanvasNavbar');
 const offcanvas = bootstrap.Offcanvas.getOrCreateInstance(offcanvasElement);
